1b4cb9b478a1a948c838d4dcbae50a8d68a00d53
AC59852 <austin.caron1@gmail.com>
2022-11-21 16:57:24 -0500

    added filters logic, componentized template for classes. added bosses to template. added computed data from json. added dynamic localstorage

---
+++ src/components/TableComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <table id="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Stage</th>
+      </tr>
+    </thead>
+    <tbody v-for="(index, label) in data" :class="`table__content table__content--${label}`" :key="index">
+      <tr class="tabel__label"><th colspan="2">{{label}}</th></tr>
+      <!-- get the data from the label and loop through it -->
+      <tr v-for="item in data[label]" :key="item.id">
+        <td>
+          <input type="checkbox" :id="item.id" @click="$emit('toggleChoice', item, $event)">
+          <a href="#" class="table__link" @click="$emit('loadContent', $event)">{{item.name}}</a>
+        </td>
+        <td>{{item.stage}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+props: ['data'],
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,27 +5,27 @@
         <router-link to="/bosses" class="selectors__link">
           <img src="/" >
           <h2>Bosses</h2>
-          <h3>Boss Count: <span>{{ bossesKilled }}</span> / <span>28</span></h3>
+          <h3>Boss Count: <span>{{ bossesKilled }}</span> / <span>{{ bosses }}</span></h3>
         </router-link>
         <router-link to="/melee" class="selectors__link">
           <img src="/" >
           <h2>Melee</h2>
-          <h3>Item Count: <span>{{ summonerItems }}</span> / <span>28</span></h3>
+          <h3>Item Count: <span>{{ obtainedMeleeItems }}</span> / <span>{{ meleeItems }}</span></h3>
         </router-link>
         <router-link to="/ranged" class="selectors__link">
           <img src="/" >
           <h2>Ranged</h2>
-          <h3>Item Count: <span>{{ summonerItems }}</span> / <span>28</span></h3>
+          <h3>Item Count: <span>{{ obtainedRangedItems }}</span> / <span>{{ rangedItems }}</span></h3>
         </router-link>
         <router-link to="/mage" class="selectors__link">
           <img src="/" >
           <h2>Mage</h2>
-          <h3>Item Count: <span>{{ summonerItems }}</span> / <span>28</span></h3>
+          <h3>Item Count: <span>{{ obtainedMageItems }}</span> / <span>{{ mageItems }}</span></h3>
         </router-link>
         <router-link to="/summoner" class="selectors__link">
           <img src="/" >
           <h2>Summoner</h2>
-          <h3>Item Count: <span>{{ summonerItems }}</span> / <span>86</span></h3>
+          <h3>Item Count: <span>{{ obtainedSummonerItems }}</span> / <span>{{ summonerItems }}</span></h3>
         </router-link>
     </section>
   </div>
@@ -33,14 +33,77 @@
 
 <script>
 // @ is an alias to /src
+import data from '@/assets/data.json'
 
 export default {
   name: 'HomeView',
 
   data() {
     return {
-      bossesKilled: 0,
-      summonerItems: 0
+      bosses: data['bosses']['pre-hardmode'].length + data['bosses'].hardmode.length + data['bosses'].event.length,
+      meleeItems: data['melee'].weapons.length + data['melee'].armour.length + data['melee'].accessories.length,
+      rangedItems: data['ranged'].weapons.length + data['ranged'].armour.length + data['ranged'].accessories.length,
+      mageItems: data['mage'].weapons.length + data['mage'].armour.length + data['mage'].accessories.length,
+      summonerItems: data['summoner'].weapons.length + data['summoner'].armour.length + data['summoner'].accessories.length,
+    }
+  },
+
+  mounted() {
+    // log all localstorage
+    for (let i = 0; i < localStorage.length; i++) {
+      console.log(localStorage.key(i))
+    }
+  },
+
+  computed: {
+    obtainedSummonerItems() {
+      // check if there is a local storage for the current route
+      if (localStorage.getItem('summonerItems')) {
+        // get the local storage and loop through it
+        return JSON.parse(localStorage.getItem('summonerItems')).length
+      } else {
+        return 0
+      }
+    },
+
+    obtainedMeleeItems() {
+      // check if there is a local storage for the current route
+      if (localStorage.getItem('meleeItems')) {
+        // get the local storage and loop through it
+        return JSON.parse(localStorage.getItem('meleeItems')).length
+      } else {
+        return 0
+      }
+    },
+
+    obtainedRangedItems() {
+      // check if there is a local storage for the current route
+      if (localStorage.getItem('rangedItems')) {
+        // get the local storage and loop through it
+        return JSON.parse(localStorage.getItem('rangedItems')).length
+      } else {
+        return 0
+      }
+    },
+
+    obtainedMageItems() {
+      // check if there is a local storage for the current route
+      if (localStorage.getItem('mageItems')) {
+        // get the local storage and loop through it
+        return JSON.parse(localStorage.getItem('mageItems')).length
+      } else {
+        return 0
+      }
+    },
+
+    bossesKilled() {
+      // check if there is a local storage for the current route
+      if (localStorage.getItem('bossesItems')) {
+        // get the local storage and loop through it
+        return JSON.parse(localStorage.getItem('bossesItems')).length
+      } else {
+        return 0
+      }
     }
   },
 
