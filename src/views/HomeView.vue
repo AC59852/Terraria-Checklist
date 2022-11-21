dd362fa2bd241e658206bc22c8efa4652d779d8a
AC59852 <austin.caron1@gmail.com>
2022-11-21 08:58:57 -0500

    added basic routing for each class, added template page that takes json data and uses it, added html for template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Terraria Tracker</h1>
+    <section id="selectors">
+        <router-link to="/bosses" class="selectors__link">
+          <img src="/" >
+          <h2>Bosses</h2>
+          <h3>Boss Count: <span>{{ bossesKilled }}</span> / <span>28</span></h3>
+        </router-link>
+        <router-link to="/melee" class="selectors__link">
+          <img src="/" >
+          <h2>Melee</h2>
+          <h3>Item Count: <span>{{ summonerItems }}</span> / <span>28</span></h3>
+        </router-link>
+        <router-link to="/ranged" class="selectors__link">
+          <img src="/" >
+          <h2>Ranged</h2>
+          <h3>Item Count: <span>{{ summonerItems }}</span> / <span>28</span></h3>
+        </router-link>
+        <router-link to="/mage" class="selectors__link">
+          <img src="/" >
+          <h2>Mage</h2>
+          <h3>Item Count: <span>{{ summonerItems }}</span> / <span>28</span></h3>
+        </router-link>
+        <router-link to="/summoner" class="selectors__link">
+          <img src="/" >
+          <h2>Summoner</h2>
+          <h3>Item Count: <span>{{ summonerItems }}</span> / <span>86</span></h3>
+        </router-link>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
+
+  data() {
+    return {
+      bossesKilled: 0,
+      summonerItems: 0
+    }
+  },
+
   components: {
-    HelloWorld
   }
 }
 </script>
---
+++ src/views/TrackerView.vue
@@ -0,0 +1,59 @@
+<template>
+  <section id="tracker">
+    <h1>{{this.route.name}} items</h1>
+    <section id="content">
+      <iframe src="https://terraria.fandom.com/wiki/Guide:Class_setups" frameborder="0" class="content__web"></iframe>
+      <section id="items">
+        <div class="items__filters">
+          <button @click="filterContent($event)">weapons</button>
+          <button @click="filterContent($event)">armour</button>
+          <button @click="filterContent($event)">accessories</button>
+        </div>
+        <table id="table">
+          <thead>
+            <tr>
+              <th>Item Name</th>
+              <th>Stage</th>
+            </tr>
+          </thead>
+          <tbody v-for="(index, label) in data" :class="`table__content table__content--${label}`" :key="index">
+            <tr class="items__label"><th colspan="2">{{label}}</th></tr>
+            <!-- get the data from the label and loop through it -->
+            <tr v-for="item in data[label]" :key="item.id">
+              <td>
+                <input type="checkbox">
+                <span>{{item.name}}</span>
+              </td>
+              <td>{{item.stage}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </section>
+  </section>
+</template>
+
+<script>
+import data from '@/assets/data.json'
+export default {
+  name: 'TrackerView',
+
+  data() {
+    return {
+      route: this.$route,
+      data: data[this.$route.name],
+    }
+  },
+
+  mounted() {
+    console.log(this.data)
+  },
+
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
